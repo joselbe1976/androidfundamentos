66b17b4b1d9d98cc6300d7d6f3674d7fbbaf5717
JOSE LUIS BUSTOS ESTEBAN <jlbustos@equilibrha.es>
2017-10-31 00:07:43 +0100

    Cambio Dos

--- app/src/main/java/com/joselbe/guedr/MainActivity.kt
+++ app/src/main/java/com/joselbe/guedr/MainActivity.kt
@@ -14,7 +14,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         Log.v(TAG,"onCreate executing")
 
-         
+        if (savedInstanceState != null){
+            //extraemos el valor
+            Log.v(TAG, "savedInstanceState no es null")
+            Log.v(TAG, "El valor recuperado es ${savedInstanceState.getString("Key1"))}"  //extraigo el valor de una clave.
+
+        }
+        else{
+            Log.v(TAG, "savedInstanceState es null")
+        }
+    }
+
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        Log.v(TAG,"Ejecuta onSaveInstanceState")
+
+        /*
+        if (outState != null){
+            // Aqui podemos star seguros que podemos llamar metodos
+            outState.putString("Key1", "Valor Joselbe")  //save values
+        }
+        */
+        outState?.putString("Key1", "Valor Joselbe") //esto es lo mismo que lo de arriba dado que es un opcional
+
     }
 
 }
